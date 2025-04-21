e174e2015af87e1890ecf7f13552069d7e7a87e9
ucxl <ucxl@qq.com>
2025-04-22 05:50:27 +0800

    update

--- busybox.mk
+++ busybox.mk
@@ -1,14 +1,13 @@
-EXTRAVERSION = -Magisk
-BB_VER = $(VERSION).$(PATCHLEVEL).$(SUBLEVEL)$(EXTRAVERSION)
+MINORLEVEL := 1
+BB_VER := "$(VERSION).$(PATCHLEVEL).$(SUBLEVEL).$(MINORLEVEL) topjohnwu"
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := busybox
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_STATIC_LIBRARIES := libselinux
-LOCAL_DISABLE_FORMAT_STRING_CHECKS := true
-LOCAL_LDFLAGS := -static -Wl,--wrap=realpath
+LOCAL_LDFLAGS := -static -Wl,--wrap=realpath -Wl,--wrap=rename -Wl,--wrap=renameat -Wl,--wrap=getaddrinfo
 LOCAL_CFLAGS := \
 -w -include include/autoconf.h -D__USE_BSD -D__USE_GNU \
--DBB_VER=\"$(BB_VER)\"
+-DBB_VER=\"$(BB_VER)\" -DBB_BT=AUTOCONF_TIMESTAMP
 
 LOCAL_SRC_FILES := \
